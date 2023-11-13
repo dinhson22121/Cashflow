3b841a0930dfff834e7098789bf41fbc2d3927b9
sngo6 <sondinh.ngo@dxc.com>
2023-11-13 17:43:37 +0700

    #10 add new feature investment

---
+++ src/main/java/com/cashmm/cashflow/investments/controller/InvestmentController.java
@@ -0,0 +1,14 @@
+package com.cashmm.cashflow.investments.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+public class InvestmentController {
+
+
+}
